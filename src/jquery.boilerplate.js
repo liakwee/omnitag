c0bdf933affb756385fe8cf3e34c200ae3bbfd4e
liakwee <liakwee.lee@xm-asia.com>
2014-08-04 16:29:22 +0800

    initial commit

--- src/jquery.boilerplate.js
+++ src/jquery.boilerplate.js
@@ -1,30 +1,16 @@
-// the semi-colon before function invocation is a safety net against concatenated
-// scripts and/or other plugins which may not be closed properly.
+
 ;
 (function ($, window, document, undefined) {
 
-  // undefined is used here as the undefined global variable in ECMAScript 3 is
-  // mutable (ie. it can be changed by someone else). undefined isn't really being
-  // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
-  // can no longer be modified.
-
-  // window and document are passed through as local variable rather than global
-  // as this (slightly) quickens the resolution process and can be more efficiently
-  // minified (especially when both are regularly referenced in your plugin).
-
   // Create the defaults once
-  var pluginName = "defaultPluginName",
+  var pluginName = "omnitag",
     defaults = {
       propertyName: "value"
     };
 
-  // The actual plugin constructor
+
   function Plugin(element, options) {
     this.element = element;
-    // jQuery has an extend method which merges the contents of two or
-    // more objects, storing the result in the first object. The first object
-    // is generally empty as we don't want to alter the default options for
-    // future instances of the plugin
     this.settings = $.extend({}, defaults, options);
     this._defaults = defaults;
     this._name = pluginName;
@@ -47,8 +33,7 @@
     }
   });
 
-  // A really lightweight plugin wrapper around the constructor,
-  // preventing against multiple instantiations
+
   $.fn[ pluginName ] = function (options) {
     this.each(function () {
       if (!$.data(this, "plugin_" + pluginName)) {
