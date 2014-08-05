9871edc95805e0e8e1ecac6c935190e5839f7759
liakwee <liakwee.lee@xm-asia.com>
2014-08-05 16:51:09 +0800

    Added manually call

--- dist/jquery.omnitag.js
+++ dist/jquery.omnitag.js
@@ -1,6 +1,6 @@
 /*
  *  jQuery omnitag - v0.0.2
- *  A Site Catalyst custom link helper
+ *  A Omniture SiteCatalyst custom link helper
  *  https://github.com/liakwee/omnitag
  *
  *  Made by Lee Liak Wee
@@ -16,7 +16,8 @@
       delay: 300,
       account: 'Site Catalyst Account',
       trackDownloadLinks: true,
-      trackExternalLinks: false
+      trackExternalLinks: false,
+      taggingData: undefined
     };
 
 
@@ -56,47 +57,51 @@
         name;
 
       data = $(this.element).data('tagging');
-      //console.log(data);
-      events = data.events;
-      eVar = data.eVar;
-      prop = data.prop;
-      value = data.value;
-
-      try{
-        if(events){
-          s.linkTrackVars = 'events,';
-          s.linkTrackEvents = events.join();
-          s.events = events.join();
-        }
-
-        if(eVar){
-          for(var i= 0; i<eVar.length; i++){
-            name = eVar[i];
-            s.linkTrackVars += (name+',');
-            s[name] = value[i];
-          }
-        }
-
-        if(prop){
-          for(var j= 0; j<prop.length; j++){
-            name = prop[j];
-            s.linkTrackVars += (name+',');
-            s[name] = value[j];
-          }
-          s.linkTrackVars = s.linkTrackVars.substr(0, s.linkTrackVars.length-1);
-        }
 
 
-
-        s.tl(this, 'o', data.linkname);
-        //console.log(s.linkTrackVars);
-        //console.log(s.linkTrackEvents);
-        s.linkTrackVars = '';
-      }
-      catch(err){
-        console.log(err);
+      if(!data){
+        data = this.settings.taggingData;
       }
 
+      console.log(data);
+      if(data){
+
+        events = data.events;
+        eVar = data.eVar;
+        prop = data.prop;
+        value = data.value;
+
+        try{
+          if(events){
+            s.linkTrackVars = 'events,';
+            s.linkTrackEvents = events.join();
+            s.events = events.join();
+          }
+
+          if(eVar){
+            for(var i= 0; i<eVar.length; i++){
+              name = eVar[i];
+              s.linkTrackVars += (name+',');
+              s[name] = value[i];
+            }
+          }
+
+          if(prop){
+            for(var j= 0; j<prop.length; j++){
+              name = prop[j];
+              s.linkTrackVars += (name+',');
+              s[name] = value[j];
+            }
+            s.linkTrackVars = s.linkTrackVars.substr(0, s.linkTrackVars.length-1);
+          }
+
+          s.tl(this, 'o', data.linkname);
+          s.linkTrackVars = '';
+        }
+        catch(err){
+          console.log(err);
+        }
+      }
     },
 
     openURI: function(link){
